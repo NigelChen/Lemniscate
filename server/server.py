b0742112c0b9db43594baa86268d9c01823b0ac0
nigelchen <nigelchen2014@gmail.com>
2015-04-02 23:06:15 -0700

    added IP banning function

--- server/server.py
+++ server/server.py
@@ -1,21 +1,33 @@
-import socket, threading,time
+import socket, threading,time, os.path
 
 host = '10.0.1.25'
 port = 1337
+
 s = socket.socket()
 repeat = 0.4
 s.bind((host,port))
-print '** SERVER IS UP ONLINE ** IP: ' + host
+print '** SERVER IS UP ONLINE ** IP: ' + str(socket.gethostbyname(socket.gethostname()))
 s.listen(5)
 users = []
 rawNames = []
 repeatNums = []
+#userNames (USERNAME => SOCKET)
 userNames = {}
+#SocketNames (SOCKET => USERNAME)
+socketNames= {}
 beginTime = -1
+bans = []
 spamTime = 0
 spamLimit = 17
-stopped = False
 firstTime = True
+
+def stripJunk(inn):
+    x = list(inn)
+    x[1]=''
+    y = str(x)
+    print y
+    return y
+
 def sendMessage(input,person):
     global users
     if type(person) is str:
@@ -24,6 +36,16 @@ def sendMessage(input,person):
     else:
         for i in range (0,len(users)):
             users[i].send(userNames[person]+': ' + input)
+
+def ban(name):
+    global bans
+    f = open('banlist.txt','w')
+    f.write(socketNames[name])
+    f.write('\n')
+    f.write('#'+name)
+    sendMessage(name + ' has been banned','s')
+    bans.append(name)
+    f.close()
 def userUpdate():
     global userNames
     global rawNames
@@ -43,15 +65,16 @@ def commandThread(connection,):
     global users
     while 1:
         x = raw_input('')
-        if x.upper() == 'STOP':
-            quit()
-        elif x.upper() == 'USERS':
+        if x.upper() == 'USERS':
             print str(len(users)) + ' user(s) in the chatroom.'
             for i in range (len(users)):
                 print rawNames[i]
         elif x.upper() == 'BROADCAST':
             y = raw_input('->')
             sendMessage(y,'fd')
+        elif x.upper() =='BAN':
+            y = raw_input('BAN->')
+            ban(y)
 x = threading.Thread(target=commandThread,args=(s,))
 x.start()
 def listenThread(connection,n):
@@ -66,53 +89,76 @@ def listenThread(connection,n):
     while 1:
         if not firstTime:
             spamTime = time.time()-beginTime
-        if stopped:
-            print 'aa'
+        try:
+            #Spam filter
+            if firstTime:
+                beginTime = time.time()
+                data = connection.recv(1024)
+                spam +=1
+                print userNames[connection] + ': ' + data
+                for i in range(0,len(users)):
+                    users[i].send(userNames[connection] + ': ' + data)
+                firstTime=False
+            elif spamTime > 5 and spam < spamLimit:
+                spamTime = 0
+                spam = 0
+                firstTime = True
+            elif (spamTime >= 5 and spam > spamLimit or spam > spamLimit):
+                print 'Muted ' + userNames[connection]
+                print '------------------------'
+                print str(spam) + ' messages in ' + str(spamTime) + ' seconds.'
+                connection.send('You have been muted for 10 seconds.')
+                time.sleep(10)
+                firstTime = True
+                spamTime = 0
+                spam  = 0
+            else:
+
+                data = connection.recv(1024)
+                if n in bans:
+                    c.send('You are BANNED from the server!')
+                    rawNames.remove(n)
+                    users.remove(connection)
+                    userNames.pop(connection, None)
+                    userUpdate()
+                    break
+                spam +=1
+                print userNames[connection] + ': ' + data
+                sendMessage(data,connection)
+        except:
+            #client disconnected. Removes all traces of user from server.
+            users.remove(connection)
+            print userNames[connection] + ' disconnected from the server.'
+            for i in range (0,len(users)):
+                users[i].send(userNames[connection] + ' disconnected from the server.\n')
+            rawNames.remove(n)
+            userNames.pop(connection, None)
+            userUpdate()
             break
-        else:
-            try:
-                #Spam filter
-                if firstTime:
-                    beginTime = time.time()
-                    data = connection.recv(1024)
-                    spam +=1
-                    print userNames[connection] + ': ' + data
-                    for i in range(0,len(users)):
-                        users[i].send(userNames[connection] + ': ' + data)
-                    firstTime=False
-                elif spamTime > 5 and spam < spamLimit:
-                    spamTime = 0
-                    spam = 0
-                    firstTime = True
-                elif (spamTime >= 5 and spam > spamLimit or spam > spamLimit):
-                    print 'Muted ' + userNames[connection]
-                    print '------------------------'
-                    print str(spam) + ' messages in ' + str(spamTime) + ' seconds.'
-                    connection.send('You have been muted for 10 seconds.')
-                    time.sleep(10)
-                    firstTime = True
-                    spamTime = 0
-                    spam  = 0
-                else:
-                    data = connection.recv(1024)
-                    spam +=1
-                    print userNames[connection] + ': ' + data
-                    sendMessage(data,connection)
-            except:
-                #client disconnected. Removes all traces of user from server.
-                users.remove(connection)
-                print userNames[connection] + ' disconnected from the server.'
-                for i in range (0,len(users)):
-                    users[i].send(userNames[connection] + ' disconnected from the server.\n')
-                rawNames.remove(n)
-                userNames.pop(connection, None)
-                userUpdate()
-                break
 
 while 1:
-    c,addr = s.accept()
+    while 1:
+        banned = False
+        if not os.path.exists('banlist.txt'):
+            open('banlist.txt','a')
+            print 'Creating banlist...'
+        c,addr = s.accept()
+        #Check if user is in banlist
+        f = open('banlist.txt','r')
+        for line in f:
+            if '#' in line:
+                pass
+            elif stripJunk(addr) in line:
+                c.send('You are BANNED from the server.')
+                banned = True
+        #does this if NOT banned. Otherwise, continue.
+        if not banned:
+            break
+        else:
+            continue
     name = c.recv(1024)
-    print rawNames
+    print addr
+    f.close()
     if name in rawNames:
         name = name + str(repeat)
         c.send('Someone else already has the same name as you. Your new name is now ' + name)
@@ -126,6 +172,7 @@ while 1:
     rawNames.append(name)
     users.append(c)
     userNames[c]=name
+    socketNames[name] = stripJunk(addr)
     #start user's personal thread
     userUpdate()
     y = threading.Thread(target=listenThread,args=(c,name))
